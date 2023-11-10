9c5b1f856b51a9e6cbc6a659e3c43d0b3193b6ea
KevinMathewEC <51322169+KevinMathewEC@users.noreply.github.com>
2023-11-10 15:59:31 +0530

    Update Train_client.py

--- Train_client.py
+++ Train_client.py
@@ -1,3 +1,6 @@
+# TCPIP Networking Lab4
+# Description: TCP Client Program in Python
+
 
 import socket
 import threading
@@ -5,17 +8,43 @@ import os
 import time
 import select
 import ssl
+import json
 
-def receive_data(ssl_socket):
-    try:
-        data = ssl_socket.recv(1024).decode()
-        while data != 'close':
-            if not data:
-                break
-            print(f"Received from server: {data}")
-            data = ssl_socket.recv(1024).decode()
-    except ConnectionRefusedError:
-        print("Connection with server has been reset")
+def receive_data(server_socket):
+
+    server_socket.listen(1)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+       # context.load_cert_chain(certfile='server.crt', keyfile='server.key')
+    while True:
+
+    # Accept a client connection
+        client_socket, client_address = server_socket.accept()
+
+    # Wrap the socket with SSL
+        server_ssl_socket = context.wrap_socket(client_socket, server_side=True)
+
+        try:
+
+        # Receive data from the client
+            data = ssl_socket.recv(1024)
+            print(f"Received data from client: {data.decode('utf-8')}")
+
+        finally:
+                # Close the SSL socket
+            ssl_socket.close()
+   #     print("Waiting for server")
+   #     data = ssl_socket.recv(1024).decode()
+   #     print("Received data from server")
+   #     while data != 'close':
+   #         data = ssl_socket.recv(1024).decode()
+   #         if not data:
+   #             break
+   #         print(f"Received from server: {data}")
+   #         print("Server data received   ")
+   #         #data = ssl_socket.recv(1024).decode()
+   #         print("Server data decoded")
+   # except ConnectionRefusedError:
+   #     print("Connection with server has been reset")
 def send_data(ssl_socket,file_path):
         try:
             # Initialize the current file size
@@ -33,8 +62,12 @@ def send_data(ssl_socket,file_path):
                     with open(file_path, 'r') as file:
                         file.seek(current_size)  # Move the file pointer to the last read position
                         new_data = file.read(size_diff)
+                       # json_data=json.dumps(new_data)
                         print("New Data:", new_data)
-                        ssl_socket.send(new_data.encode('utf-8'))
+                        current_timestamp = int(time.time())  # UNIX timestamp
+                        message = f"Timestamp: {current_timestamp}\nData: {new_data}"
+                        print("Message: ",message)
+                        ssl_socket.send(message.encode('utf-8'))
 
                 # Update the current file size
                     current_size = new_size
@@ -46,23 +79,39 @@ def send_data(ssl_socket,file_path):
 
 
 if __name__ == '__main__':
-    server_ip = "10.217.61.237"
-    port = 5000  # socket server port number
+    server_ip = "10.217.61.237"#"HandsomePi"
+    port = 65432  # socket server port number
+    hostname = socket.gethostname()
+    server_port = 5000
+
+    # Get the local IP address
+    local_server_ip = socket.gethostbyname(hostname)
+    print("local_server_ip",local_server_ip)
+    
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the server's IP and port
+    server_socket.bind((local_server_ip, server_port))
+
     # Create an SSL context
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    ssl_context.check_hostname = False
-   # ssl_context.load_verify_locations('client-cert.pem')
-    ssl_context.verify_mode = ssl.CERT_NONE  # You can set this to ssl.CERT_OPTIONAL or ssl.CERT_REQUIRED for certificate verification
-
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # instantiate
-    ssl_socket = ssl_context.wrap_socket(client_socket, server_hostname=server_ip)
+  #  ssl_context.check_hostname = False
+    ssl_context.load_verify_locations('client.crt')
+#    ssl_context.verify_mode = ssl.CERT_REQUIRED  # You can set this to ssl.CERT_OPTIONAL or ssl.CERT_REQUIRED for certificate verification
+    print("client socket")
+    train_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # instantiate
+    ssl_socket = ssl_context.wrap_socket(train_client_socket, server_hostname=server_ip)
     ssl_socket.connect((server_ip, port))  # connect to the server
 
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Define the path to the text file you want to monitor
     file_path = "RFID_data.txt"
+    print("file found")
 # Create two separate threads for sending and receiving data
-    receive_thread = threading.Thread(target=receive_data, args=(ssl_socket,))
+    receive_thread = threading.Thread(target=receive_data, args=(server_socket,))
     send_thread = threading.Thread(target=send_data, args=(ssl_socket,file_path))
+
+
 
 # Start both threads
     receive_thread.start()
@@ -73,3 +122,41 @@ if __name__ == '__main__':
     send_thread.join()
 
     ssl_socket.close()
+
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the server's IP and port
+    server_socket.bind((local_server_ip, server_port))
+
+    # Create an SSL context
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+  #  ssl_context.check_hostname = False
+    ssl_context.load_verify_locations('client.crt')
+#    ssl_context.verify_mode = ssl.CERT_REQUIRED  # You can set this to ssl.CERT_OPTIONAL or ssl.CERT_REQUIRED for certificate verification
+    print("client socket")
+    train_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # instantiate
+    ssl_socket = ssl_context.wrap_socket(train_client_socket, server_hostname=server_ip)
+    
+    ssl_socket.connect((server_ip, port))  # connect to the server
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Define the path to the text file you want to monitor
+    file_path = "RFID_data.txt"
+    print("file found")
+# Create two separate threads for sending and receiving data
+    receive_thread = threading.Thread(target=receive_data, args=(server_socket,))
+    send_thread = threading.Thread(target=send_data, args=(ssl_socket,file_path))
+
+
+
+# Start both threads
+    receive_thread.start()
+    send_thread.start()
+
+# Wait for both threads to finish
+    receive_thread.join()
+    send_thread.join()
+
+    ssl_socket.close()
+                                                                                                            
